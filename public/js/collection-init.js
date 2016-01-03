0ef6e36556b422411a9c746a64cffb46c9996691
Anton-Permnenev <anton.permenev@gmail.com>
2016-01-03 15:49:37 +0300

    templates and  collection page changed

---
+++ public/js/collection-init.js
@@ -0,0 +1,7 @@
+$(document).ready(
+    $(function () {
+            $("#navbar").load("./template.html #navbar-template")      ;
+            $("#welcome").load("./template.html #welcome-template")      ;
+            $(".calendar").load("./calendar.html");
+        }
+    ));
--- public/js/main.js
+++ public/js/main.js
@@ -3,20 +3,19 @@
  */
 $(document).ready(
     $(function () {
-            $("#single-test").append($('<div>').
-                    load("../src/template.html #test")
-            );
+            $("#navbar").load("./template.html #navbar-template")      ;
+            $("#welcome").load("./template.html #welcome-template")      ;
             var result;
-            $.ajax({
-                url: 'http://188.166.36.174:3000/api/themes',
-                dataType: 'json',
-                success: function (responce) {
-                    console.log(responce);
-                    result = responce;
-                    $("#test>.img-title").text(result[0].description);
-                    //$("#single-test>.theme-img").attr("src",result[0].image_url);
-                }
-            });
+            //$.ajax({
+            //    url: 'http://188.166.36.174:3000/api/themes',
+            //    dataType: 'json',
+            //    success: function (responce) {
+            //        console.log(responce);
+            //        result = responce;
+            //        $("#test>.img-title").text(result[0].description);
+            //        //$("#single-test>.theme-img").attr("src",result[0].image_url);
+            //    }
+            //});
 
 
         }
