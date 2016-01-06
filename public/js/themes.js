85c6d2a81da81bff89838666638de249fabcac15
Anton-Permnenev <anton.permenev@gmail.com>
2016-01-06 17:56:57 +0300

    calendar changed to datePicker

---
+++ public/js/themes.js
@@ -0,0 +1,11 @@
+$(document).ready(
+    $(function () {
+            $("#navbar").load("./template.html #navbar-template");
+            $("#welcome").load("./template.html #welcome-template");
+            $("#new-themes").load("./template.html #new-themes");
+        }
+
+
+    ));
+
+
